fbc7e25fba44ceb7619228083b28fae89e51d483
KAZI MUSTAFIJUR RAHAMAN <kmrofficial786@gmail.com>
2026-03-15 12:39:42 +0530

    Add matrix transpose program in C

---
+++ transpose_matrix.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, c, i, j;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    int matrix[r][c];
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < r; i++)
+        for(j = 0; j < c; j++)
+            scanf("%d", &matrix[i][j]);
+
+    printf("Transpose of matrix:\n");
+
+    for(i = 0; i < c; i++)
+    {
+        for(j = 0; j < r; j++)
+        {
+            printf("%d ", matrix[j][i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
