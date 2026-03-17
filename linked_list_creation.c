6c26ab9964573f9402eb49d73b31b2c12ff61494
KAZI MUSTAFIJUR RAHAMAN <kmrofficial786@gmail.com>
2026-03-17 08:56:25 +0530

    Add linked list creation program in C

---
+++ linked_list_creation.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+int main()
+{
+    struct Node *head = NULL, *temp, *newNode;
+    int n, i;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        newNode = (struct Node*) malloc(sizeof(struct Node));
+
+        printf("Enter data: ");
+        scanf("%d", &newNode->data);
+
+        newNode->next = NULL;
+
+        if(head == NULL)
+        {
+            head = temp = newNode;
+        }
+        else
+        {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    printf("Linked list created successfully.\n");
+
+    return 0;
+}
